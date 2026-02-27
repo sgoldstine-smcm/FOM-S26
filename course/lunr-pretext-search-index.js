e67cc5463528ffa24a6a0d2b55b1132d2c58e3b6
sgoldstine-smcm <sgoldstine@smcm.edu>
2026-02-27 04:56:30 +0000

    Latest build deployed.

--- course/lunr-pretext-search-index.js
+++ course/lunr-pretext-search-index.js
@@ -17,6 +17,69 @@ var ptx_lunr_docs = [
   "number": "",
   "title": "Quiz 1 Review Solutions",
   "body": " Quiz 1 Review Solutions    Foundations of Mathematics       Let , , and .    Compute .      Remark.  on its own is a correct identification of , and so are and .    Compute .     (Note that you can answer this question without computing .)    Compute .  Since , .    Is ? Explain your answer.  No, because but .  (Note that you can apply the same argument to or .)    Is ? Explain your answer.  No, because but . On the other hand, .  It may (or may not) help to observe that .      Let , , , and .  Determine whether each of the following statements is true or false.       true       false ( is not a set, so it’s not a subset of  .)       false       true (equivalently, .)       false (the elements of  are integers, while the elements of P(S) are sets of integers.)       false       true       true       true       true      Consider the following intervals in , which will be our universal set for this question.   Find the following sets. If the set is an interval, write it in interval notation.                                    The figure below shows a subset of of the form where  and  are subsets of  . The endpoints of the line segment and the edges of the rectangle are all in the subset. (The and axes are not in the subset.)   A set in the x y plane consisting of a rectangle and a line segment. The rectangle includes all the points with x values between 1 and 4 and y values between -2 and -3. The line segment is horizontal and goes from the point (1,-4) to the point (4,-4). There are extra axes to the right of the graph and below the graph as described in the text.     What is  ? What is  ?      Remark. Either of the formulas for  is valid; you don’t need both of them. But note that . Do you see the difference?       Give the set by listing its elements between set braces.         Consider the sets   Here are five open sentences about a set  . For each, list the all the values of  (chosen from the options , , and ) that make the sentence true. For example, if I gave you the open sentence , the answer would be: , .)  As a reminder, denotes the set of integers.     .        .        has exactly two elements.   ,      .        .         For the purposes of this question, the math wing is having an open house on Thursday and Friday. Let  be the statement Kiko is coming to the open house on Thursday and let  be the statement Kiko is coming to the open house on Friday.   Rewrite each of the following using only the symbols , , , , , , , and without changing its meaning.       Kiko is coming to both days of the open house.       Kiko is coming to at most one day of the open house. (Equivalently, Kiko is coming to no more than one day of the open house.)           (Any of these three answers is valid. There are other possibilities, too.)    Kiko is coming to the open house on Friday if she doesn’t come on Thursday.     (Or, if you want to be too clever by half, .)      A look at some conditional statements and their converses.    For this part, will only take on integer values.  Consider the statement If is even, then is even.   Is the statement true or false?  It is true. (Sketch of the proof: Suppose  is even. By definition, there is an integer  for which . It follows that , which is even because .)  Give the converse of the statement. Is the converse true or false?  If is even, then is even.  This is true. (The proof here is trickier, but we’ll be ready for it in a few weeks.)    Consider the statement If , then .   Is the statement true or false?  It is false. For instance, , but .  Give the converse of the statement. Is the converse true or false?  If , then   This is true. This is just another way of saying that .      "
+},
+{
+  "id": "homework-4",
+  "level": "1",
+  "url": "homework-4.html",
+  "type": "Worksheet",
+  "number": "",
+  "title": "Axioms of <span class=\"process-math\">\\({\\bf R}\\)<\/span> and <span class=\"process-math\">\\({\\bf Z}\\)<\/span>",
+  "body": " Axioms of and    Foundations of Mathematics    The following statements are all things that you can take for granted when you write proofs in this course. In practice, you’ll probably be fine without consulting this list, but it can be instructive to work out how you use these properties when you do basic arithmetic and algebra.   Axioms of  that (mostly) also apply to   Operations  Note that you can always assume the results of basic arithmetic; for instance, you do not need to prove that . In addition, we have the following axioms.     is closed under addition . (Likewise, is closed under addition.)  For all , . (For all , .)     is closed under multiplication . (Likewise, is closed under multiplication.)  For all , . (For all , .)    While this can be derived from the previous two axioms, it is worth noting that both  and  are also closed under subtraction.    (Note: you cannot replace  with  in this one, but the list would feel incomplete without it.)  For all , if , then .    ‘    Algebra    Addition is commutative in  (and in  ).  For all , .    Addition is associative in  (and in  ).  For all , .    Multiplication is commutative in  (and in  ).  For all , .    Multiplication is associative in  (and in  ).  For all , .    Multiplication distributes over addition in  (and in  ).  For all , .  This is also called the distributive property, and it is the basis of all the factoring and expanding you’ve done since high school algebra.    Not really an axiom of its own, but a corollary to the standard distributive property: multiplication also distributes over subtraction in  (and in  ).  For all , .    This rule, which applies to everything in  and  , is the reason why we spend so much time in math classes thinking about how to factor algebraic expressions.  For all , if , then or .    While the next cluster of properties is already covered by the blanket statement that you don’t have to prove the results of simple additions and multiplications and by the other axioms from Operations , I’m including them here because of their significance in group theory, one of the major components of abstract algebra.    The number  is the additive identity of  (and of  ).  For all , .    Every element of  has an additive inverse in  . (Likewise, every element of  has an additive inverse in  .)  For all , , where . (For all , , where .)    The number  is the multiplicative identity of  (and of  ).  For all , .    Every nonzero element of  has a multiplicative inverse in  .  For all , if , then and .   Remarks. Recall from exponent rules that . I have my reasons for writing the property using the exponential notation.  Notice that  is conspicuously absent here. Do you see why?      Inequalities  Everything in this section applies to  in exactly the same way it applies to  , so I will skip over saying and for everything here.    Suppose . If , then .  You can replace with , , or . In fact, if you note that if , then , and also remember that is synonymous with , you can derive each of these three variations from the original statement.    Not really a new axiom, since you can replace with in the axiom above, but it is also true that for all , if , then .    Suppose and .  If , then .  If , then .  You can replace with , and it isn’t too difficult to reword this axiom for the assumption that .    Suppose .  If and , then .  This is sometimes called the transitive property of  . The transitive property also applies to , , and .     Trichotomy :  For any , exactly one of the following three statements is true.        Remark. An alternate form of trichotomy is the statement that for every , is exactly one of the following three things: positive, , or negative. This version can be combined with the first axiom in this section to derive the version comparing  to  .       Axioms particular to    Mostly just the one axiom     The Well Ordering Principle :  Every non-empty subset of has a smallest element.   Remarks. This is somewhat subtler than the other axioms in this list. To have a better understanding of its significance, you might want to think through why the corresponding statement is false for or for .  There is a logically equivalent axiom that we’ll learn about in a few more chapters.    Here is a statement you may also assume in FOM, but it is actually a provable consequence of the Well Ordering Principle. The proof is an interesting exercise that you might want to attempt— after Chapter 6.    There are no integers between  and  .  In other words, if , then or .    A potentially useful corollary of the previous statement:  Suppose . If , then .    Finally, you may use the Division Algorithm (stated in Chapter 1 and again in Chapter 4) without proof. In Section 1.9, the book outlines how the Division Algorithm can be proven from the Well Ordering Principle.     Division Algorithm :  Suppose and . Then there exist unique integers  and  such that and .      "
+},
+{
+  "id": "operations-3",
+  "level": "2",
+  "url": "homework-4.html#operations-3",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "closed under addition closed under multiplication cannot "
+},
+{
+  "id": "algebra-2",
+  "level": "2",
+  "url": "homework-4.html#algebra-2",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "commutative associative distributes distributive "
+},
+{
+  "id": "algebra-4",
+  "level": "2",
+  "url": "homework-4.html#algebra-4",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "additive identity additive inverse multiplicative identity nonzero "
+},
+{
+  "id": "inequalities-3",
+  "level": "2",
+  "url": "homework-4.html#inequalities-3",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "transitive Trichotomy "
+},
+{
+  "id": "mostly-just-the-one-axiom-2",
+  "level": "2",
+  "url": "homework-4.html#mostly-just-the-one-axiom-2",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "The Well Ordering Principle "
+},
+{
+  "id": "mostly-just-the-one-axiom-6",
+  "level": "2",
+  "url": "homework-4.html#mostly-just-the-one-axiom-6",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Division Algorithm "
 }
 ]
 
